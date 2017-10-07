f839a45d9d88a4f8f16051359244eae4be8588c4
Unay Santisteban <davidu@softcom.com>
2017-10-08 00:48:29 +0200

    More tests added.

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -13,9 +13,37 @@ class ContainerTest extends \PHPUnit\Framework\TestCase
         ));
 
         $this->assertTrue($container->has('api'));
+        $this->assertEquals('some api key', $container->get('api'));
+
         $this->assertTrue($container->has('std'));
         $this->assertInstanceOf('\stdClass', $container->get('std'));
         $this->assertEquals($std, $container->get('std'));
+
+        $this->assertCount(2, $container);
+    }
+
+    public function testSet()
+    {
+        $container = new \OtherCode\Container\Container();
+        $container->set('one', 1);
+        $container->offsetSet('two', 2);
+        $container['three'] = 3;
+        $container->four = 4;
+
+        $this->assertCount(4, $container);
+
+        $this->assertTrue($container->has('one'));
+        $this->assertEquals(1, $container->get('one'));
+
+        $this->assertTrue($container->has('two'));
+        $this->assertEquals(2, $container->get('two'));
+
+        $this->assertTrue($container->has('three'));
+        $this->assertEquals(3, $container->get('three'));
+
+        $this->assertTrue($container->has('four'));
+        $this->assertEquals(4, $container->get('four'));
+
 
     }
 }
